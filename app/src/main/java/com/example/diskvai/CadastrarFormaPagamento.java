24a75d620c8cf09b2119fdddee957294a8f78c2c
Alex <alvesalexal@gmail.com>
2019-06-04 18:27:59 -0300

    cadastrar_forma

---
+++ app/src/main/java/com/example/diskvai/CadastrarFormaPagamento.java
@@ -0,0 +1,13 @@
+package com.example.diskvai;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastrarFormaPagamento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastrar_forma_pagamento);
+    }
+}
