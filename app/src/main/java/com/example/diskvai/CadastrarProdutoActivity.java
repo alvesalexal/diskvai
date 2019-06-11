deccc9d3a8c90e82c335127f70a91b6605456df1
isaaccarvalho <isaac.oliv.carvalho@gmail.com>
2019-06-11 12:06:42 -0300

    Fix Listar Entregador, Funçao Carregar imagem

--- app/src/main/java/com/example/diskvai/CadastrarProdutoActivity.java
+++ app/src/main/java/com/example/diskvai/CadastrarProdutoActivity.java
@@ -1,31 +1,48 @@
 package com.example.diskvai;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.StrictMode;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Base64;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 public class CadastrarProdutoActivity extends AppCompatActivity {
 
-    private EditText nome, descricao, preco, frete;
-    private TextView precoFinal;
+    private int PICK_IMAGE_REQUEST = 1;
+
+    private EditText nome, descricao, preco;
+
     private String id_empresa, resposta;
     private String a[]={"#"};
+    private ImageView imageView;
+    private Bitmap bitmap;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +62,7 @@ public class CadastrarProdutoActivity extends AppCompatActivity {
         nome = findViewById(R.id.nome);
         descricao = findViewById(R.id.descricao);
         preco = findViewById(R.id.preco);
+        imageView = findViewById(R.id.imgProduto);
     }
 
     public void back(View view) {
@@ -108,24 +126,45 @@ public class CadastrarProdutoActivity extends AppCompatActivity {
     public void cadastrarProduto(View view) {
         if (validaCadastro()) {
             try {
+
+
                 StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
                 StrictMode.setThreadPolicy(policy);
 
                 OkHttpClient client = new OkHttpClient();
 
-                HttpUrl.Builder urlBuilder = HttpUrl.parse("http://gabriellacastro.com.br/disk_vai/inserirProduto.php").newBuilder();
-                urlBuilder.addQueryParameter("nome_produto", nome.getText().toString());
-                urlBuilder.addQueryParameter("url_foto", "");
-                urlBuilder.addQueryParameter("id_empresa", id_empresa);
-                urlBuilder.addQueryParameter("desc_produto", descricao.getText().toString());
-                urlBuilder.addQueryParameter("preco", preco.getText().toString());
+                String uploadImage = getStringImage(bitmap);
+
+                final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/png");
 
 
-                String url = urlBuilder.build().toString();
-
-                Request request = new Request.Builder()
-                        .url(url)
+                RequestBody multiPartBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("foto",uploadImage)
+                        .addFormDataPart("nome_produto", nome.getText().toString())
+                        .addFormDataPart("id_empresa", id_empresa)
+                        .addFormDataPart("desc_produto", descricao.getText().toString())
+                        .addFormDataPart("preco", preco.getText().toString())
                         .build();
+                Request request = new Request.Builder()
+                        .url("http://gabriellacastro.com.br/disk_vai/inserirProduto.php")
+                        .post(multiPartBody)
+                        .build();
+
+
+
+//                HttpUrl.Builder urlBuilder = HttpUrl.parse("http://gabriellacastro.com.br/disk_vai/inserirProduto.php").newBuilder();
+//                urlBuilder.addQueryParameter("nome_produto", nome.getText().toString());
+//                urlBuilder.addQueryParameter("foto", "");
+//                urlBuilder.addQueryParameter("id_empresa", id_empresa);
+//                urlBuilder.addQueryParameter("desc_produto", descricao.getText().toString());
+//                urlBuilder.addQueryParameter("preco", preco.getText().toString());
+//
+//                String url = urlBuilder.build().toString();
+//
+//                Request request = new Request.Builder()
+//                        .url(url)
+//                        .build();
 
                 client.newCall(request).enqueue(new Callback() {
                     @Override
@@ -168,4 +207,34 @@ public class CadastrarProdutoActivity extends AppCompatActivity {
     private void alert(String valor) {
         Toast.makeText(this, valor, Toast.LENGTH_SHORT).show();
     }
+
+    public void showFileChooser(View view) {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getStringImage(Bitmap bmp){
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
 }
